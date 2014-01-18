395307ff27027403c17ce6ceec0b06af59841f08
Tim Whitlock <r-bitbucket@timwhitlock.info>
2014-01-18 17:40:32 +0000

    fixed deps warning

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,8 @@
  * PHPUnit bootstrap for Loco tests.
  */
 
-if ( ! file_exists( dirname(__DIR__) . '/composer.lock') ) {
-    die("Dependencies must be installed using composer:\n\nphp composer.phar install\n\n"
-        . "See http://getcomposer.org for help with installing composer\n");
+if ( ! file_exists(dirname(__DIR__).'/vendor') ) {
+    die("\nDependencies must be installed using composer:\nSee http://getcomposer.org\n\n");
 }
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
