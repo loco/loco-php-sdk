6165a61bfa9adbabeb1cc5d3820df235f66c1cec
Tim Whitlock <r-bitbucket@timwhitlock.info>
2014-01-18 14:00:40 +0000

    added phpunit config and ping test

--- src/Loco/Api/ApiClient.php
+++ src/Loco/Api/ApiClient.php
@@ -2,6 +2,37 @@
 
 namespace Loco\Api;
 
-class ApiClient {
+
+use Guzzle\Common\Collection;
+use Guzzle\Plugin\Oauth\OauthPlugin;
+use Guzzle\Service\Client;
+use Guzzle\Service\Description\ServiceDescription;
+
+
+class ApiClient extends Client {
+
     
+    /**
+     * @return ApiClient
+     */
+    public static function factory( $config = array() ){
+       
+        // Provide a hash of default client configuration options
+        $default = array (
+            'base_url' => 'https://localise.biz/api',
+            'key' => '',
+        );
+
+        // No values are currently required when creating the client
+        $required = array ();
+
+        // Merge in default settings and validate the config
+        $config = Collection::fromConfig( $config, $default, $required );
+
+        // Create a new instance of self
+        $client = new self( $config->get('base_url'), $config );
+
+        return $client;
+    }    
+
 }
--- tests/Loco/Tests/Api/ApiClientTest.php
+++ tests/Loco/Tests/Api/ApiClientTest.php
@@ -3,16 +3,38 @@
 namespace Loco\Tests\Api;
 
 use Loco\Api\ApiClient;
+use Guzzle\Service\Builder\ServiceBuilder;
+
 
 /**
- * Simple client test
+ * Test the API client works.
  */
 class ApiClientTest extends \PHPUnit_Framework_TestCase {
     
     
-    public function testClassExists(){
+    public function testClassValid(){
         $client = new ApiClient('http://example.com/');
+        $this->assertInstanceOf('\Guzzle\Service\Client', $client );
+    }
+    
+    
+    public function testServiceBuilder(){
+        $jsonfile = __DIR__.'/../../../../services.json';
+        $this->assertFileExists( $jsonfile, 'Config not copied from services.json.dist' );
+        $builder = ServiceBuilder::factory( $jsonfile );
+        $client = $builder->get('loco_test');
         $this->assertInstanceOf('\Loco\Api\ApiClient', $client );
+        return $client;
+    }
+    
+
+    /**
+     * @depends testServiceBuilder
+     */    
+    public function testPing( ApiClient $client ){
+        $request = $client->get('ping.json');
+        $response = $request->send();
+        $this->assertContains( 'pong', $response->json() );
     }
     
     
