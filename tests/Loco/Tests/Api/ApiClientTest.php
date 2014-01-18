e2841e90c7c5d69b0284e731b705a99bf2ed2e61
Tim Whitlock <r-bitbucket@timwhitlock.info>
2014-01-18 19:35:44 +0000

    added live and fake groups to test suite

--- tests/Loco/Tests/Api/ApiClientTest.php
+++ tests/Loco/Tests/Api/ApiClientTest.php
@@ -8,6 +8,7 @@ use Guzzle\Service\Builder\ServiceBuilder;
 
 /**
  * Test the API client works.
+ * Skip tests requiring internet connection with --exclude-group live
  */
 class ApiClientTest extends GuzzleTestCase {
     
@@ -25,6 +26,7 @@ class ApiClientTest extends GuzzleTestCase {
 
     /**
      * Live ping test via overloaded service method
+     * @group live
      */
     public function testPing(){
         $client = $this->getServiceBuilder()->get('loco');
@@ -37,6 +39,7 @@ class ApiClientTest extends GuzzleTestCase {
     /**
      * Live 404 test via custom endpoint
      * @expectedException \Guzzle\Http\Exception\BadResponseException
+     * @group live
      */    
     public function testNotFound(){
         $client = $this->getServiceBuilder()->get('loco');
@@ -46,6 +49,7 @@ class ApiClientTest extends GuzzleTestCase {
     
     /**
      * Live file converter test
+     * @group live
      */
     public function testConverter(){
         $client = $this->getServiceBuilder()->get('loco');
@@ -58,6 +62,23 @@ class ApiClientTest extends GuzzleTestCase {
         ) );
         $this->assertInstanceOf('\Loco\Api\Response\ConvertResponse', $result );
         $this->assertRegExp( '/msgid\s+"foo"\s+msgstr\s+"bar"/', (string) $result );
+    }
+    
+    
+    /**
+     * Fake ping over Guzzle test server
+     * @group fake
+     */
+    public function testFakePing(){
+        // set up fake response for ping
+        $fake = json_encode( array('ping'=>'pong') );
+        $http = "HTTP/1.1 200 OK\r\nContent-Length: ".strlen($fake)."\r\n\r\n".$fake;
+        $this->getServer()->enqueue( $http );
+        // call Ping()
+        $client = $this->getServiceBuilder()->get('loco');
+        $client->setBaseUrl( $this->getServer()->getUrl() );
+        $pong = (string) $client->Ping();
+        $this->assertEquals( 'pong', $pong );
     }
 
     
