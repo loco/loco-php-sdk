04e540a6290b0976390078bef8ebee6a097e81fc
Tim Whitlock <r-bitbucket@timwhitlock.info>
2014-01-19 18:22:43 +0000

    switched to xlf/yml example

--- example/convert.php
+++ example/convert.php
@@ -1,21 +1,21 @@
 <?php
 /**
- * Convert an iOS Localizable.strings file to an Android XML file
+ * Example XLIFF -> YAML conversion via the Loco REST API.
+ * Extracts a single language from a XLIFF file and renders it to a single YAML file.
  */
 require __DIR__.'/../vendor/autoload.php';
+
 use Loco\Http\ApiClient;
+use Loco\Http\Response\ConvertResponse;
 
 $client = ApiClient::factory();
 
+/* @var $result ConvertResponse */
 $result = $client->Convert( array(
-    'src'    => file_get_contents(__DIR__.'/sample.strings'),
-    'from'   => 'strings',
-    'to'     => 'xml',
-    'format' => 'android',
-    'locale' => 'fr',
-    'domain' => 'test',
+    'src'    => file_get_contents(__DIR__.'/sample.xlf'),
+    'from'   => 'xlf',
+    'to'     => 'yml',
+    'locale' => 'es',
 ) );
 
-$xml = (string) $result;
-
-echo $xml;
+echo (string) $result;
--- example/ping.php
+++ example/ping.php
@@ -3,8 +3,13 @@
  * Ping the API, just to check everything is working.
  */
 require __DIR__.'/../vendor/autoload.php';
+
 use Loco\Http\ApiClient;
+use Guzzle\Service\Resource\Model;
 
 $client = ApiClient::factory();
-$pong = $client->Ping();
-echo $pong, "\n";
+
+/* @var $result Model */
+$result = $client->Ping();
+
+echo $result->get('ping'), "\n";
