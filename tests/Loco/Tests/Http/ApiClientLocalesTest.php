ba3a384fd29a8cc68fe736443c5094cf3471231a
Tim Whitlock <r-bitbucket@timwhitlock.info>
2014-02-11 22:45:28 +0000

    altered test for root array primative

--- tests/Loco/Tests/Http/ApiClientLocalesTest.php
+++ tests/Loco/Tests/Http/ApiClientLocalesTest.php
@@ -3,6 +3,7 @@
 namespace Loco\Tests\Http;
 
 use Loco\Http\ApiClient;
+use Guzzle\Service\Resource\Model;
 
 /**
  * Test the live /locales API.
@@ -15,11 +16,11 @@ class ApiClientLocalesTest  extends ApiClientTest {
     public function testLocalesList(){
         $client = $this->getClient();
         $list = $client->locales();
-        $this->assertInternalType( 'array', $list );
-        //$this->assertCount( 9, $list ); // <- samples project
+        // model would be a typed array model, except not supported in Guzzle.
+        //$this->assertInstanceOf( '\Guzzle\Service\Resource\Model', $list );
+        $this->assertInternalType('array', $list );
+        
         $locale = $list[0]; 
-        // @todo see how Guzzle implements model containers ??
-        // https://github.com/wordnik/swagger-core/wiki/Datatypes
         $this->assertArrayHasKey('code', $locale );
     }
     
