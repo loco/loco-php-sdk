82e3d1fc27916f3a13f6356aed3a548ff6d954eb
Tim Whitlock <r-bitbucket@timwhitlock.info>
2014-02-16 16:47:45 +0000

    added strict command for response validation

---
+++ src/Loco/Http/Command/StrictResponseParser.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Loco\Http\Command;
+
+use Guzzle\Service\Command\OperationResponseParser;
+use Guzzle\Service\Command\LocationVisitor\VisitorFlyweight;
+
+/**
+ * Response parser that enables schema to be injected into response models.
+ */
+class StrictResponseParser extends OperationResponseParser {
+
+    /** 
+     * Singleton
+     * @var StrictResponseParser
+     */
+    protected static $instance;
+    
+    /**
+     * Get singleton
+     * @return StrictResponseParser
+     */
+    public static function getInstance(){
+        if( ! static::$instance ) {
+            static::$instance = new StrictResponseParser( VisitorFlyweight::getInstance(), true );
+        }
+        return static::$instance;
+    }
+    
+    
+}
