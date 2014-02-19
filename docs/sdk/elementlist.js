e6da7a809dd1ad2e2b88f63cfd38190c3f96197b
Tim Whitlock <r-bitbucket@timwhitlock.info>
2014-02-19 11:56:16 +0000

    apigen docs update

--- docs/sdk/elementlist.js
+++ docs/sdk/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Loco\\Http\\ApiClient"],["c","Loco\\Http\\Response\\RawResponse"],["c","Loco\\Http\\Response\\ZipResponse"]];
+ApiGen.elements = [["c","Loco\\Http\\ApiClient"],["c","Loco\\Http\\Command\\StrictCommand"],["c","Loco\\Http\\Command\\StrictResponseParser"],["c","Loco\\Http\\Response\\RawResponse"],["c","Loco\\Http\\Response\\ZipResponse"]];
