e5e5dc70d5f34f6f7bab377ca87cd9a06f821096
Tim Whitlock <r-bitbucket@timwhitlock.info>
2015-06-09 10:59:36 +0100

    rebuilt import command

--- src/Console/Command/Generated/ImportCommand.php
+++ src/Console/Command/Generated/ImportCommand.php
@@ -19,11 +19,12 @@ class ImportCommand extends Command {
         $this
             ->setName( 'loco:import' )
             ->setMethod( 'import' )
-            ->setDescription( 'Import from language pack files' )
+            ->setDescription( 'Import assets and translations from a language pack file' )
             ->addArgument('ext',InputArgument::OPTIONAL,'Import file type specified as a file extension','json')
             ->addOption('src','',InputOption::VALUE_REQUIRED,'Raw source of file being imported','{}')
             ->addOption('index','',InputOption::VALUE_REQUIRED,'Specify whether file indexes translations by asset ID or source texts',null)
             ->addOption('locale','',InputOption::VALUE_REQUIRED,'Specify target locale if importing translations',null)
+            ->addOption('async','',InputOption::VALUE_REQUIRED,'Specify that import should be done asyncronously',null)
             ->addOption('key','k',InputOption::VALUE_OPTIONAL,'Override configured API key for this request','')
         ;
     }
