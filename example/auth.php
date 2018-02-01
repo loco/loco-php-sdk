8d078de03e50282b12d7bb4f0a1e8429d198aa9e
supersmile2009 <supersmile2009@gmail.com>
2018-02-01 19:18:17 +0200

    Fixed examples

--- example/auth.php
+++ example/auth.php
@@ -4,11 +4,10 @@
  */
 require __DIR__.'/../vendor/autoload.php';
 
-// Our project key is in a config file so we instantiate the client via the Guzzle service builder
-$client = Guzzle\Service\Builder\ServiceBuilder::factory( __DIR__.'/../config.json' )->get('loco');
+$client = Loco\Http\ApiClient::factory(['key' => 'your-api-key']);
 
-/* @var $result \Guzzle\Service\Resource\Model */
+/** @var \GuzzleHttp\Command\Result $result */
 $result = $client->authVerify();
 
-printf("Authenticated as '%s'\n", $result['user']['name'] );
-printf("Project name is '%s'\n",  $result['project']['name'] );
+printf("Authenticated as '%s'\n", $result['user']['name']);
+printf("Project name is '%s'\n", $result['project']['name']);
