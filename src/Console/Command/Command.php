efc1be6143f4e83ec71f265a2db69e172a4b0dd4
supersmile2009 <supersmile2009@gmail.com>
2018-02-02 17:00:15 +0200

    Command refactoring. Broken. Middleware?

--- src/Console/Command/Command.php
+++ src/Console/Command/Command.php
@@ -2,124 +2,124 @@
 
 namespace Loco\Console\Command;
 
+use GuzzleHttp\Command\Result;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Middleware;
+use Loco\Console\Application;
+use Psr\Http\Message\RequestInterface;
 use Symfony\Component\Console\Command\Command as BaseCommand;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputDefinition;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-
-use Guzzle\Common\Event;
-use Guzzle\Http\Message\Response;
-use Guzzle\Http\Message\EntityEnclosingRequest;
-use Guzzle\Service\Resource\Model;
-use Guzzle\Service\Description\Parameter;
-use Guzzle\Http\Exception\BadResponseException;
-use Guzzle\Service\Command\ResponseClassInterface;
-
-use Loco\Http\ApiClient;
 
 
 /**
  * Base command for all loco API calls
+ *
+ * @method Application getApplication()
  */
-abstract class Command extends BaseCommand {
-    
+abstract class Command extends BaseCommand
+{
+
     /**
      * @var string
-     */    
+     */
     private $method;
-
 
     /**
      * Set the callable name of the magic service method
+     *
      * @return Command
      */
-    protected function setMethod( $method ){
+    protected function setMethod($method)
+    {
         $this->method = $method;
+
         return $this;
     }
 
-
-
     /**
      * Execute call to endpoint
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @throws \GuzzleHttp\Exception\BadResponseException
      */
-    protected function execute( InputInterface $input, OutputInterface $output ){
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
         $args = $input->getArguments() + $input->getOptions();
 
         // override API key
-        if( isset($args['key']) && ( $apiKey = trim($args['key']) ) ){
-            $client = $this->getApplication()->getRestClient( $apiKey );
-        }
-        // else ensure key is unset so default it used
+        if (isset($args['key']) && ($apiKey = trim($args['key']))) {
+            $client = $this->getApplication()->getRestClient($apiKey);
+        } // else ensure key is unset so default it used
         else {
             $client = $this->getApplication()->getRestClient();
-            unset( $args['key'] );
+            unset($args['key']);
         }
         // noisy output if -v or higher
         $verbosity = $output->getVerbosity();
-        if( OutputInterface::VERBOSITY_NORMAL < $verbosity ){
-            $output->writeln( sprintf('Calling <comment>%s</comment>', $this->method) );
+        if (OutputInterface::VERBOSITY_NORMAL < $verbosity) {
+            $output->writeln(sprintf('Calling <comment>%s</comment>', $this->method));
             // print request/response if -vv or higher
-            if( OutputInterface::VERBOSITY_VERBOSE < $verbosity ){
+            if (OutputInterface::VERBOSITY_VERBOSE < $verbosity) {
+
                 // inspect request before sending
-                $dispatcher = $client->getEventDispatcher();
-                $dispatcher->addListener('request.before_send', function( Event $e )use( $output ){
-                    $request = $e['request'];
-                    /* @var $request EntityEnclosingRequest */
-                    $output->writeln( sprintf('Requesting <comment>%s</comment>', $request->getPath() ) );
-                    $lines = explode( "\n", trim( $request->__toString() ) );
-                    echo ' > ',implode("\n > ", $lines ),"\n";
-                } );
-                // inspect response after receiving
-                $dispatcher->addListener('request.sent', function( Event $e )use( $output ){
-                    $response = $e['response'];
-                    /* @var $response Response */
-                    $output->writeln( sprintf('Responded <comment>%u</comment>', $response->getStatusCode() ) );
-                    $lines = explode( "\n", trim( $response->__toString() ) );
-                    echo ' < ',implode("\n < ", $lines ),"\n";
-                } );
-            }
-        }        
-        // call overloaded function and show body on error
-        try {
-            $result = $client->__call( $this->method, array( $args ) );
-            // print result unless -q
-            if( OutputInterface::VERBOSITY_QUIET < $output->getVerbosity() ){
-                $this->showResult( $result, $output );
+                $client->getHandlerStack()->push(
+                    Middleware::mapRequest(
+                        function (RequestInterface $request) use ($output) {
+                            $output->writeln(sprintf('Requesting <comment>%s</comment>', $request->getRequestTarget()));
+//                            $lines = explode("\n", trim($request->__toString()));
+//                            echo ' > ', implode("\n > ", $lines), "\n";
+                        }
+                    )
+                );
+
+//                 inspect response after receiving
+                $client->getHandlerStack()->push(
+                    Middleware::mapResponse(
+                        function (ResponseInterface $response) use ($output) {
+                            $output->writeln(sprintf('Responded <comment>%u</comment>', $response->getStatusCode()));
+//                            $lines = explode("\n", trim($response->__toString()));
+//                            echo ' < ', implode("\n < ", $lines), "\n";
+                        }
+                    )
+                );
             }
         }
-        catch( BadResponseException $e ){
+        // call overloaded function and show body on error
+        try {
+            $result = $client->{$this->method}($args);
+            // print result unless -q
+            if (OutputInterface::VERBOSITY_QUIET < $output->getVerbosity()) {
+                $this->showResult($result, $output);
+            }
+        } catch (BadResponseException $e) {
             $output->writeln('<comment>Bad response:</comment>');
-            $output->writeln( (string) $e->getResponse()->getBody() );
+            $output->writeln((string)$e->getResponse()->getBody());
             throw $e;
         }
     }
 
-
-
     /**
      * Overridable default shows result on successful api call
-     * @param Guzzle\Service\Resource\Model | array
+     *
+     * @param Result|array $result
+     * @param OutputInterface $output
+     *
      * @return void
-     */    
-    protected function showResult( $result, OutputInterface $output ){
-        if( OutputInterface::VERBOSITY_NORMAL < $output->getVerbosity() ){
+     */
+    protected function showResult($result, OutputInterface $output)
+    {
+        if (OutputInterface::VERBOSITY_NORMAL < $output->getVerbosity()) {
             $output->writeln('<info>'.$this->getName().' OK</info>');
         }
-        if( $result instanceof Model ){
+        if ($result instanceof Result) {
             $result = $result->toArray();
         }
-        if( is_array($result) ){
-            if( defined('JSON_PRETTY_PRINT') ){
-                echo json_encode( $result, JSON_PRETTY_PRINT ),"\n";
-            }
-            else {
-                print_r( $result );
-            }
-        }
-        else {
+        if (is_array($result)) {
+            echo json_encode($result, JSON_PRETTY_PRINT), "\n";
+        } else {
             echo $result;
         }
     }
